b9207301c9c4cfd63d723f868ab17ea64eb3c224
Ye <ocean.yef@gmail.com>
2022-09-29 13:35:51 +0800

    Initial commit

---
+++ import_section_table.py
@@ -0,0 +1,374 @@
+import numpy as np
+import sqlite3
+import test8_ortools_stock_cutter_1d
+import copy
+
+import argparse
+#import tkinter as tk
+#from tkinter import filedialog
+
+#root = tk.Tk()
+#root.withdraw()
+
+
+
+#Folderpath = filedialog.askdirectory()
+#ssss = r"Z:\数字化课题\结构数字化课题\02 工作文件\2021 11 22 套料优化py脚本\2021 12 29 套料优化更新\Tekla_NCX_database.db"
+
+ssss = r"Tekla_NCX_database.db"
+
+
+cnR = sqlite3.connect(ssss)
+print("Opened database successfully")
+
+section=[]
+length=[]
+id=[]
+material=[]
+weight=[]
+numb=[]
+c = cnR.cursor()
+cursor1 = c.execute("SELECT Section,Length,ID_list,Material,Weight,Number_list from Import_table")
+for row in cursor1:
+    section.append(row[0])
+    length.append(int(row[1])) # length is described with int
+    id.append(row[2])
+    material.append(row[3])
+    weight.append(round(row[4],2))
+    numb.append(row[5])
+cnR.commit()
+
+arr_section = np.array(section)
+arr_length = np.array(length)
+arr_id = np.array(id)
+arr_material = np.array(material)
+arr_weight = np.array(weight)
+arr_numb = np.array(numb)
+
+arr_data = [arr_section,arr_length,arr_id,arr_material,arr_weight]
+arr_data2=np.transpose(arr_data)
+
+
+
+# dt=np.dtype([('','S100')])
+data = np.empty([len(section), 5], dtype='S100')
+
+
+# for i in range(len(section)):
+#     # print(type(section[i]))
+#     # print(type(length[i]))
+#     # print(type(id[i]))
+#     #
+#     # print((section[i]))
+#     # print((length[i]))
+#     # print((id[i]))
+#
+#
+#     data[i][0]=(section[i])
+#     data[i][1]=(length[i])
+#     data[i][2]=(id[i])
+#     #data[i][2]=bytes(temp1,encoding='utf-8')
+#
+
+
+
+
+# for i_data in range(len(data)):
+#     for j_data in range(len(data[0])):
+#         temp_data  =  data[i_data][j_data]
+#         temp_data1 = temp_data.decode('utf-8')
+#         data[i_data][j_data]=temp_data1
+#
+# print('----bbbb-----')
+# print(np.shape(data))
+# print(type(data[0][0]))
+# print(type(data[0][1]))
+# print(type(data[0][2]))
+#
+# print((data[0][0]))
+# print((data[0][1]))
+# print((data[0][2]))
+#
+# print(data[0])
+# print((data[0][:]))
+#
+p = r'input_section_table.txt'
+outfile = open ("out_member_group.txt","w")
+# #
+# #
+# with open(p,encoding = 'utf-8') as f:
+#     data_org = np.loadtxt(f,str,delimiter = " ")
+#     #print(data_org[:])
+#
+#
+#
+# print("-----------qq-------")
+# print(type(data_org[0][0]))
+# print(type(data_org[0][1]))
+# print(type(data_org[0][2]))
+# print(np.shape(data_org))
+
+data = arr_data2
+
+
+
+
+section_list=np.unique(data[:,0])
+
+
+def One_dim_list(ele):
+    # 将输入的ele list 转换为1维数组，便于索引
+    c_dot = 0
+    len_ele = len(ele)
+    key_ele_list = []
+    for i_ele in ele:
+        if(str(i_ele).count(","))>0:
+    #        c_dot = c_dot + str(i_id).count(",")
+            key_ele_list.extend(i_ele.split(","))
+        else:
+            key_ele_list.append(str(i_ele))
+    #print(len_id+c_dot)
+    #print(len(key_id_list))
+    return(key_ele_list)
+
+def pair_id_numb(k_id):
+    k=[]
+    if isinstance(k_id,int):
+        k = str(k_id)
+    else:
+        for ki in k_id.split(","):
+            print(key_id.index(ki))
+            k.append(key_numb[key_id.index(ki)])
+    print(type(k))
+    return(k)
+
+def Select_Section(section,data):
+    (ni,nj)=list(np.shape(data))
+#    print((ni,nj))
+    del_i=[]
+    data = np.insert(data, nj, 0, axis=1)
+    for i in range(0,ni):
+#        print(" check",data[i])
+        if data[i][0] != section:
+            # print(data[i])
+            # print(i)
+            del_i.append(i)
+            #data1=np.delete(data,[i],0)
+            #print("delete",del_i)
+        else:
+#            print(type(str(data[i][nj-1])))
+            data[i][nj]=(str(data[i][2])).count(",")+1 # the quanity of bars is recorded in the last index
+            #print("keep:",i)
+    data1=np.delete(data,del_i,0)
+    (tni,tnj)=list(np.shape(data1))
+
+
+    return(data1)
+
+
+def width_tol(w,tol):
+    # print(w)
+    w = [i+tol for i in w]
+    # print(w)
+    return(w)
+
+def substi_len(val_len,len1,mat1,wei1):
+
+    # print("val_len",val_len)
+    # print("len1",len1)
+    val_len=(int(val_len))
+    len1=list(map(int, list(len1)))
+    ii = len1.index(val_len)
+
+    return [mat1[ii],wei1[ii]]
+
+str1="********* 3mm tolerance is considered in each member ******\n"
+outfile.write(str1)
+
+section=[]
+ratio=[]
+unused=[]
+lenlist=[]
+idlist=[]
+matlist=[]
+weightlist=[]
+
+
+
+#-----id 和 numb 建立匹配关系，用于索引构件编号(number_list)------#
+
+global key_id
+global key_numb
+key_id = One_dim_list(id)
+key_numb= One_dim_list(numb)
+
+print(key_id)
+print(key_numb)
+#-----------------------------------------------------------#
+
+
+
+for s in section_list:
+    section_data = Select_Section(s, data)
+    # print(section_data)
+    print("-----------\n-----------\n","Section",s,":\n-----------\n",)
+    [w1,ID1,b1,mat1,wei1]=[section_data[:,1],section_data[:,2],section_data[:, -1],section_data[:,3],section_data[:,4]]
+    # demand_sub_rolls_lenth_list, ID_list, demand_sub_rolls_quantity, material_list, weight_list
+
+    w1=list(map(int,list(w1)))
+    b1=list(map(int,list(b1)))
+
+    # print("w1:",w1)
+    # print("b1:",b1)
+
+
+
+    w1=width_tol(w1,3) # 3mm is tol
+
+    str1="------------\n"+"Section: "+str(s)+":\n"
+    outfile.write(str1)
+
+
+    ID2= list(ID1)
+    ID3=ID2
+    for i_ID2 in range(0,len(ID2)):
+
+        ID3[i_ID2]=ID2[i_ID2].split(",")
+    # print("ID3[0]",ID3[0])
+   # print("ID2_[0][0]",ID2[0][0])
+   #  print("ID3_len",len(ID3[0]))
+   #  print("ID3_type", type(ID3))
+
+    # for i_ID1 in range(0,len(ID1)):
+    #     ID1[i_ID1]=ID1[i_ID1].tostring().split(",")
+    #     print("type_i_ID1",type(ID1[i_ID1]))
+    # for i_ID3 in ID1:
+    #     print("type_i_ID3",type(i_ID3))
+    #
+    # for si in range(0,len(w1)):
+    #     print("Length:",w1[si],"ID:",ID1[si])
+
+
+
+
+    #test2_MIP.CSP_MIP(w1, b1)
+    #test3_gurobipy.CSP_gurobipy(w1,b1,ID1)
+    [consumed_big_rolls, consumed_sub_rolls,demand_sub_rolls]=test8_ortools_stock_cutter_1d.CSP_ortools(w1,b1,ID3)
+    # print(rst1)
+    # print(rst2)
+    # print(type(w[1]))
+    # print(type(w))
+    # print(b)
+    # print("big",consumed_big_rolls[0])
+    # print("sub",consumed_sub_rolls[0])
+    consumed_sub_rolls_weight = copy.deepcopy(consumed_sub_rolls)
+    consumed_sub_rolls_material = copy.deepcopy(consumed_sub_rolls)
+
+
+
+    for ic in range(len(consumed_sub_rolls_material)):
+        for jc in range(len(consumed_sub_rolls_material[ic][0])):
+            i_len = consumed_sub_rolls[ic][0][jc]
+            #print("i_len",i_len)
+            [consumed_sub_rolls_material[ic][0][jc],consumed_sub_rolls_weight[ic][0][jc]]= substi_len(i_len,w1,mat1,wei1)
+
+
+    # print("---weight group22----")
+    # print("length mat",len(consumed_sub_rolls_material))
+    # print(consumed_sub_rolls_material)
+
+
+    outfile.write("Usage info+Length_arrangement:"+str(consumed_big_rolls)+"\n")
+    outfile.write("ID_Group:"+str(demand_sub_rolls).replace("[[","[").replace("]]","]").replace("', '",",").replace("'","").replace("[[","(").replace("]]",")").replace("], [","),(")+"\n")
+    # print("------------------------")
+    # print(consumed_big_rolls)
+    # print(demand_sub_rolls)
+    # print("------------------------")
+
+    for i in range(len(consumed_big_rolls)):
+        temp1=consumed_big_rolls[i]
+        section.append(s)
+        ratio.append(temp1[0])
+        unused.append(temp1[1])
+
+        temp2 = ''
+        for i in range(len(temp1[2])):
+            temp2 = temp2 + str(temp1[2][i]-3) + ","
+        lenlist.append(temp2[0:-1])
+
+
+
+    for j in range(len(demand_sub_rolls)):
+        temp3 = ''
+        temp4 = ''
+        temp5 = ''
+        for i in range(len(demand_sub_rolls[j][0])):
+            temp3=temp3+demand_sub_rolls[j][0][i]+","
+            temp4=temp4+consumed_sub_rolls_material[j][0][i]+","
+            temp5=temp5+consumed_sub_rolls_weight[j][0][i]+","
+        idlist.append(temp3[0:-1])
+        matlist.append(temp4[0:-1])
+        weightlist.append(temp5[0:-1])
+outfile.close()
+
+
+
+
+
+
+
+tbl1 = []
+for tt in range(9):
+    tbl1.append([])
+for i in range(0, len(section)):
+    tbl1[0].append("'"+section[i]+"'")
+    tbl1[1].append(i+1)
+    tbl1[2].append(ratio[i])
+    tbl1[3].append(unused[i])
+    tbl1[4].append("'"+str(lenlist[i])+"'")
+    tbl1[5].append("'"+str(idlist[i])+"'")
+    tbl1[6].append("'"+str(matlist[i])+"'")
+    tbl1[7].append("'"+str(weightlist[i])+"'")
+    tbl1[8].append("'"+str(pair_id_numb(idlist[i])).replace("[","").replace("]","").replace("'","")+"'")
+#    print(pair_id_numb(idlist[i]))
+tbl1_T = list(zip(*tbl1))
+
+print("--------")
+print(tbl1[8])
+print(tbl1[8][0])
+print("--------")
+
+cnR = sqlite3.connect(ssss)
+cnR.execute("drop table if exists Output_table;")
+cnR.execute('''
+CREATE TABLE Output_table
+    (
+    Section           STRING,
+    Bar_group           INTEGER,
+    Usage_ratio           REAL,
+    Unusage_Length           REAL,
+    Length_List           STRING,
+    ID_List           STRING,    
+    Material_List           STRING,
+    Weight_List           STRING,
+    Number_List           STRING);''')
+cnR.commit()
+sql_insert = "INSERT INTO Output_table(Section,Bar_group,Usage_ratio,Unusage_Length,Length_List,ID_List,Material_List,Weight_List,Number_List) VALUES"
+sql_values = ""
+sql_values1 = ""
+for i in range(len(tbl1_T)):
+    a = []
+    List = tbl1_T[i]
+    for j in range(len(List)):
+        s = str(List[j])
+        a.append(s)
+    for k in range(0, len(a)):
+        sql_values += a[k]
+        sql_values += ","
+    sql_values1 = "(" + sql_values.strip(',') + ")"
+    sql_todo = sql_insert + sql_values1
+    cuY = cnR.cursor()
+    cuY.execute(sql_todo)
+    sql_values = ""
+    sql_value1 = ""
+cnR.commit()
---
+++ rolling_cutting_method.py
@@ -0,0 +1,110 @@
+import copy
+from import_section_table import args
+
+child = [5000, 5100, 5200, 5300, 5400, 5500]
+ID = [12345, 32312, 312312, 323222, 23332, 32132]
+
+if args>0:
+    parent_length = args.length
+else:
+    parent_length = 12000
+
+
+print("-****************************",parent_length)
+
+global gap_cut
+global gap_bar
+gap_cut = 3
+gap_bar = 0
+
+
+
+pos_child = []
+pos_bar = []
+
+
+def sum_list(list1, loc_of_sum_list1):
+    total = 0
+    for i in range(loc_of_sum_list1):
+        total = list1[i] + total
+    return total
+
+def insert_bar_cut(pos,temp_child,gap_bar):
+    bar = round(pos / parent_length,0)
+    for i in range(len(temp_child)):
+        if temp_child[i] > pos:
+            temp_child[i] = temp_child[i]+bar*gap_bar
+    return temp_child
+
+def split_bar(pos_child_barcut,pos_bar,bar_group):
+    bar = []
+    bar_last = []
+    for i in range(len(bar_group)):
+        temp = []
+        if i>0:
+            temp.append(pos_bar[i-1])
+        if i == 0:
+            temp.extend(pos_child_barcut[0:bar_group[0]])
+        else:
+            i_start = bar_group[i-1]
+            i_end = bar_group[i]
+            temp.extend(pos_child_barcut[i_start:i_end])
+        temp.append(pos_bar[i])
+        bar.append(temp)
+    bar_last.append(pos_bar[-2])
+    bar_last.extend(pos_child_barcut[bar_group[-1]:])
+    bar.append(bar_last)
+
+    for i in range(len(bar)):
+        bar_start = bar[i][0]
+        for j in range(len(bar[i])):
+            bar[i][j]=bar[i][j]-bar_start
+
+
+    return bar
+
+#构件分段，不计整料分缝
+for i in range(len(child)+1):
+    if i == 0:
+        pos_child.append(0)
+    else:
+        pos_child.append(sum_list(child, i)+gap_cut/2+(i-1)*gap_cut)
+
+#统计总根数
+numb_bar = round((pos_child[-1]+len(child)*gap_bar)/parent_length)+1
+
+#考虑整料分缝
+pos_child_barcut = copy.deepcopy(pos_child)
+for i in range(1,numb_bar):
+    pos_child_barcut = insert_bar_cut(parent_length*(i), pos_child_barcut, gap_bar)
+    pos_bar.append(parent_length*i+gap_cut*(i-1)+gap_cut/2)
+
+#整合切断点位置
+bar_group=[]
+for i in range(len(pos_bar)):
+    for j in range(len(pos_child_barcut)):
+        if pos_child_barcut[j]>pos_bar[i]:
+            bar_group.append(j)
+            break
+
+#拆分整料
+bar = split_bar(pos_child_barcut,pos_bar,bar_group)
+
+#-----考虑构件分段，每根零件间隙3mm-----#
+print('考虑构件分段',sum_list(child, len(child)))
+
+#-----考虑构件分段，每根零件间隙3mm-----#
+print('考虑构件分段切割锯缝 pos_child = ',pos_child)
+
+#-----考虑整料分段，每根整料间隙3mm-----#
+print('考虑整料切割锯缝 pos_child_barcut=',pos_child_barcut)
+
+#-----整料切断位置-------#
+print('考虑整料切段点 pos_bar=',pos_bar)
+
+#------锯料损耗---------#
+print('锯料损耗',(numb_bar-1)*gap_bar+(len(child)-1)*gap_cut)
+
+#------整料分组结果-----#
+print('整料分组',bar_group)
+print('分组长度',bar)
